c325290eda3e801fbcdb4f5d8e9a858dcb958fc0
KK09020 <kiku09020@gmail.com>
2022-09-26 22:46:45 +0900

    修正、改良
    ・PlayerPrefs関係のコードを削除
    ・無駄な処理部分を削除

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,5 +1,5 @@
 [System.Serializable]
-public class Rank_JsonData {
-    const int rankCnt = 3;
+public class SaveData {
+    public const int rankCnt = 3;
     public int[] rank = new int[rankCnt];
 }
